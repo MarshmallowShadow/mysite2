bcb910c670f9525fbf3bfc48e48cac2b23f4fe12
MarshmallowShadow <choiboseung@naver.com>
2022-06-03 13:51:30 +0900

    2022-06-03

---
+++ src/com/javaex/controller/UserController.java
@@ -0,0 +1,92 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.javaex.dao.UserDao;
+import com.javaex.util.WebUtil;
+import com.javaex.vo.UserVo;
+
+
+@WebServlet("/user")
+public class UserController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		UserDao uDao = new UserDao();
+		String action = request.getParameter("action");
+		
+		if("joinForm".equals(action)) {
+			WebUtil.forward(request, response, "/WEB-INF/views/user/joinForm.jsp");
+		}
+		else if("join".equals(action)) {
+			String id = request.getParameter("id");
+			String password = request.getParameter("password");
+			String name = request.getParameter("name");
+			String gender = request.getParameter("gender");
+			
+			UserVo uVo = new UserVo(id, password, name, gender);
+			System.out.println(uVo);
+			uDao.userInsert(uVo);
+			
+			WebUtil.forward(request, response, "/WEB-INF/views/user/joinOk.jsp");
+		}
+		else if("loginForm".equals(action)) {
+			WebUtil.forward(request, response, "/WEB-INF/views/user/loginForm.jsp");
+		}
+		else if("login".equals(action)) {
+			String id = request.getParameter("id");
+			String password = request.getParameter("password");
+			
+			UserVo uVo = new UserVo(id, password);
+			UserVo authUser = uDao.getUser(uVo);
+			
+			if(authUser == null) {
+				WebUtil.forward(request, response, "/WEB-INF/views/user/loginForm.jsp");
+			} else {
+				System.out.println("로그인 성공");
+				
+				HttpSession session = request.getSession();
+				session.setAttribute("authUser", authUser);
+				
+				WebUtil.redirect(request, response, "./main?");
+			}
+		}
+		else if("modifyForm".equals(action)) {
+			WebUtil.forward(request, response, "/WEB-INF/views/user/modifyForm.jsp");
+		}
+		else if("modify".equals(action)) {
+			String name = request.getParameter("name");
+			String password = request.getParameter("password");
+			String gender = request.getParameter("gender");
+			int no = Integer.parseInt(request.getParameter("no"));
+			
+			UserVo uVo = new UserVo();
+			uVo.setName(name);
+			uVo.setPassword(password);
+			uVo.setGender(gender);
+			uVo.setNo(no);
+			
+			uDao.update(uVo);
+			
+			WebUtil.redirect(request, response, "./main?");
+		}
+		else if("logout".equals(action)) {
+			HttpSession session = request.getSession();
+			session.removeAttribute("authUser");
+			
+			WebUtil.redirect(request, response, "./main?");
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/javaex/dao/UserDao.java
@@ -0,0 +1,161 @@
+package com.javaex.dao;
+
+import java.io.*;
+import java.util.*;
+import java.sql.*;
+import com.javaex.vo.UserVo;
+
+public class UserDao {
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String id = "webdb";
+	private String pw = "webdb";
+	
+	private void getConnection() {
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, id, pw);
+		} catch(ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch(SQLException e) {
+			System.out.println("error: " + e);
+		}
+	}
+	
+	private void close() {
+		try {
+			if(rs != null) {
+				rs.close();
+			} if(pstmt != null) {
+				pstmt.close();
+			} if(conn != null) {
+				conn.close();
+			}
+		} catch(SQLException e) {
+			System.out.println("error: " + e);
+		}
+	}
+	
+	public int userInsert(UserVo uVo) {
+		int count = -1;
+		
+		try {
+			getConnection();
+			
+			String query = "";
+			query += " insert into users";
+			query += " values (seq_users_no.nextval, ?, ?, ?, ?)";
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, uVo.getId());
+			pstmt.setString(2, uVo.getPassword());
+			pstmt.setString(3, uVo.getName());
+			pstmt.setString(4, uVo.getGender());
+			
+			count = pstmt.executeUpdate();
+			
+			System.out.println(count + "건이 등록되었습니다.");
+			
+		} catch(SQLException e) {
+			System.out.println("error: " + e);
+		}
+		
+		close();
+		return count;
+	}
+	
+	public int update(UserVo uVo) {
+		int count = -1;
+		
+		try {
+			getConnection();
+			
+			String query = "";
+			query += " update	users";
+			query += " set		passsword = ?,";
+			query += " 			name = ?,";
+			query += " 			gender = ?";
+			query += " where no = ?";
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(2, uVo.getPassword());
+			pstmt.setString(3, uVo.getName());
+			pstmt.setString(4, uVo.getGender());
+			pstmt.setInt(1, uVo.getNo());
+			
+			count = pstmt.executeUpdate();
+			
+			System.out.println(count + "건이 수정되었습니다.");
+			
+		} catch(SQLException e) {
+			System.out.println("error: " + e);
+		}
+		
+		close();
+		return count;
+	}
+	
+	public int delete(int no) {
+		int count = -1;
+		
+		try {
+			getConnection();
+			
+			String query = "";
+			query += " delete from users";
+			query += " no = ?";
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, no);
+			
+			count = pstmt.executeUpdate();
+			
+			System.out.println(count + "건이 식제되었습니다.");
+			
+		} catch(SQLException e) {
+			System.out.println("error: " + e);
+		}
+		
+		close();
+		return count;
+	}
+	
+	public UserVo getUser(UserVo uVo) {
+		UserVo authUser = null;
+		
+		try {
+			getConnection();
+			
+			String query = "";
+			query += " select	no,";
+			query += " 			name";
+			query += " from users";
+			query += " where id = ?";
+			query += " and password = ?";
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, uVo.getId());
+			pstmt.setString(2, uVo.getPassword());
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				int no = rs.getInt(1);
+				String name = rs.getString(2);
+				
+				authUser = new UserVo();
+				authUser.setNo(no);
+				authUser.setName(name);
+				authUser.setId(uVo.getId());
+			}
+		} catch(SQLException e) {
+			System.out.println("error: " + e);
+		}
+		
+		close();
+		return authUser;
+	}
+}
